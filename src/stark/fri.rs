5a0e1fbdf0182654c1a88af8fc596eb623bb71ac
VGLoic <vincent.genod.loic@gmail.com>
2025-10-20 11:54:37 +0200

    remove recomputation of units at reduction step

--- src/stark/fri.rs
+++ src/stark/fri.rs
@@ -29,7 +29,7 @@ pub fn low_degree_proof<const N: u32>(
 
     let mut diag_evaluations = values;
     let mut diagonal_commitments_tree = original_commitments_tree;
-    let mut units_in_row = units.to_owned();
+    let mut row_dimension = units.len();
     let mut seed = seed.unwrap_or(*diagonal_commitments_tree.root_hash());
 
     let original_commitments_root = *diagonal_commitments_tree.root_hash();
@@ -37,11 +37,11 @@ pub fn low_degree_proof<const N: u32>(
     let mut row_excluded_indices = excluded_indices.clone();
 
     let mut indirect_commitments: Vec<IndirectCommitment<N>> = vec![];
-    for _ in 0..indirect_steps_count {
+    for reduction_level in 0..(indirect_steps_count as usize) {
         let x_c_index = *pseudo_random_select_units_indices(
             &seed,
             1,
-            units_in_row.len(),
+            row_dimension,
             &row_excluded_indices,
             None,
         )
@@ -49,24 +49,24 @@ pub fn low_degree_proof<const N: u32>(
         .unwrap();
         let mut column_excluded_indices = HashSet::new();
         for i in &row_excluded_indices {
-            column_excluded_indices.insert(i % (units_in_row.len() / 4));
+            column_excluded_indices.insert(i % (row_dimension / 4));
         }
         let row_indices = pseudo_random_select_units_indices(
             &seed,
             ROW_COUNT,
-            units_in_row.len() / 4,
+            row_dimension / 4,
             &column_excluded_indices,
             Some(x_c_index),
         );
-        let x_c = &units_in_row[x_c_index];
+        let x_c = &units[scale_index(x_c_index, reduction_level)];
 
         let mut column_evaluations = vec![];
-        for unit_index in 0..(units_in_row.len() / 4) {
+        for unit_index in 0..(row_dimension / 4) {
             let mut row_points = vec![];
             let mut row_values = vec![];
             for i in 0..4 {
-                let index = (unit_index + i * units_in_row.len() / 4) % units_in_row.len();
-                row_points.push(units_in_row[index]);
+                let index = (unit_index + i * row_dimension / 4) % row_dimension;
+                row_points.push(units[scale_index(index, reduction_level)]);
                 row_values.push(diag_evaluations[index].v);
             }
             let row_interpolated_p =
@@ -83,7 +83,7 @@ pub fn low_degree_proof<const N: u32>(
         for row_index in row_indices {
             let row_diagonal_commitments = (0..4)
                 .map(|i| {
-                    let index = (row_index + i * units_in_row.len() / 4) % units_in_row.len();
+                    let index = (row_index + i * row_dimension / 4) % row_dimension;
                     diagonal_commitments_tree.select_commitment(index)
                 })
                 .collect::<Result<Vec<Commitment<N>>, anyhow::Error>>()?;
@@ -104,17 +104,17 @@ pub fn low_degree_proof<const N: u32>(
             column_commitments: indirect_column_commitments,
         });
 
+        row_dimension /= 4;
         diagonal_commitments_tree = column_commitments_tree;
         seed = *diagonal_commitments_tree.root_hash();
         diag_evaluations = column_evaluations;
-        units_in_row = units_in_row.into_iter().skip(3).step_by(4).collect();
         row_excluded_indices = column_excluded_indices;
     }
 
     let final_indices = pseudo_random_select_units_indices(
         diagonal_commitments_tree.root_hash(),
         DIRECT_COMMITMENTS_COUNT,
-        units_in_row.len(),
+        row_dimension,
         &row_excluded_indices,
         None,
     );
@@ -142,7 +142,8 @@ pub fn verify_low_degree_proof<const N: u32>(
     let mut seed: [u8; 32] = seed.unwrap_or(proof.original_commitments_root);
     let mut diag_root = proof.original_commitments_root;
 
-    let mut units_in_row = units.to_owned();
+    // let mut units_in_row = units.to_owned();
+    let mut row_dimension = units.len();
     let mut row_excluded_indices = excluded_indices.clone();
 
     if indirect_steps_count as usize != proof.indirect_commitments.len() {
@@ -152,11 +153,11 @@ pub fn verify_low_degree_proof<const N: u32>(
         ));
     }
 
-    for indirect_commitment in &proof.indirect_commitments {
+    for (reduction_level, indirect_commitment) in proof.indirect_commitments.iter().enumerate() {
         let x_c_index = *pseudo_random_select_units_indices(
             &seed,
             1,
-            units_in_row.len(),
+            row_dimension,
             &row_excluded_indices,
             None,
         )
@@ -164,23 +165,16 @@ pub fn verify_low_degree_proof<const N: u32>(
         .unwrap();
         let mut column_excluded_indices = HashSet::new();
         for i in &row_excluded_indices {
-            column_excluded_indices.insert(i % (units_in_row.len() / 4));
+            column_excluded_indices.insert(i % (row_dimension / 4));
         }
         let row_indices = pseudo_random_select_units_indices(
             &seed,
             ROW_COUNT,
-            units_in_row.len() / 4,
+            row_dimension / 4,
             &column_excluded_indices,
             Some(x_c_index),
         );
-        let x_c = &units_in_row[x_c_index];
-
-        let units_in_column: Vec<PrimeFieldElement<N>> = units_in_row
-            .iter()
-            .skip(3)
-            .step_by(4)
-            .map(|v| v.to_owned())
-            .collect();
+        let x_c = &units[scale_index(x_c_index, reduction_level)];
 
         if indirect_commitment.column_commitments.len()
             != indirect_commitment.diagonal_commitments.len()
@@ -199,16 +193,19 @@ pub fn verify_low_degree_proof<const N: u32>(
                 .zip(&indirect_commitment.column_commitments)
                 .zip(row_indices)
         {
-            let expected_four_exp_value = &units_in_row[expected_row_index].exp(4);
+            let expected_four_exp_value =
+                &units[scale_index(expected_row_index, reduction_level)].exp(4);
 
-            let mut row_points = vec![];
-            let mut row_values = vec![];
-            let row_unit = &units_in_column[associated_column_commitment.unit_index];
+            let row_unit =
+                &units[scale_index(associated_column_commitment.unit_index, reduction_level + 1)];
             if row_unit != expected_four_exp_value {
                 return Err(anyhow!(
                     "invalid unit of the column commitment, expected {expected_four_exp_value}, got {row_unit}"
                 ));
             }
+
+            let mut row_points = vec![];
+            let mut row_values = vec![];
             for row_commitment in diag_row_commitments {
                 verify_proof(
                     &diag_root,
@@ -216,7 +213,7 @@ pub fn verify_low_degree_proof<const N: u32>(
                     &row_commitment.proof,
                 )?;
 
-                let unit = &units_in_row[row_commitment.unit_index];
+                let unit = &units[scale_index(row_commitment.unit_index, reduction_level)];
                 if &unit.exp(4) != expected_four_exp_value {
                     return Err(anyhow!(
                         "the power of 4 of the commitment unit point must be equal to the column unit"
@@ -248,7 +245,7 @@ pub fn verify_low_degree_proof<const N: u32>(
             }
         }
 
-        units_in_row = units_in_column;
+        row_dimension /= 4;
         diag_root = indirect_commitment.column_root;
         seed = indirect_commitment.column_root;
         row_excluded_indices = column_excluded_indices;
@@ -257,7 +254,7 @@ pub fn verify_low_degree_proof<const N: u32>(
     let final_indices = pseudo_random_select_units_indices(
         &diag_root,
         DIRECT_COMMITMENTS_COUNT,
-        units_in_row.len(),
+        row_dimension,
         &row_excluded_indices,
         None,
     );
@@ -288,7 +285,12 @@ pub fn verify_low_degree_proof<const N: u32>(
             ));
         }
 
-        points.push(units_in_row[direct_commitment.unit_index]);
+        points.push(
+            units[scale_index(
+                direct_commitment.unit_index,
+                proof.indirect_commitments.len(),
+            )],
+        );
         values.push(direct_commitment.evaluation.v);
     }
 
@@ -336,4 +338,12 @@ fn derive_indirect_steps_count(max_degree: u32) -> Result<u32, anyhow::Error> {
     }
 
     Ok(i)
+}
+
+fn scale_index(index: usize, reduction_level: usize) -> usize {
+    let mut result = index;
+    for _ in 0..reduction_level {
+        result = 4 * result + 3;
+    }
+    result
 }
