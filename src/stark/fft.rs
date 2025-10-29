7cc0a4770bdde0750c18797fbea09560bd77bf9a
VGLoic <vincent.genod.loic@gmail.com>
2025-10-29 17:09:40 +0100

    implement inverse fft

--- src/stark/fft.rs
+++ src/stark/fft.rs
@@ -1,3 +1,5 @@
+use anyhow::anyhow;
+
 use crate::primefield::PrimeFieldElement;
 
 /// Performs the Fourier Transform over a whole set of units generated as successive power of a generator.
@@ -59,4 +61,71 @@ pub fn fft<const N: u32>(
     }
 
     [first_half_result, second_half_result].concat()
+}
+
+#[allow(dead_code)]
+pub fn inverse_fft<const N: u32>(
+    values: &[PrimeFieldElement<N>],
+    units: &[PrimeFieldElement<N>],
+) -> Result<Vec<PrimeFieldElement<N>>, anyhow::Error> {
+    if values.is_empty() {
+        return Ok(vec![0.into(); units.len()]);
+    }
+    let ft = fft(values, units);
+    let len_as_u32: u32 = units
+        .len()
+        .try_into()
+        .map_err(|_| anyhow!("units are too long, can not express the length as u32"))?;
+    let inv_len = PrimeFieldElement::<N>::from(len_as_u32)
+        .inv()
+        .ok_or(anyhow!("unable to inverse the length of units"))?;
+
+    let mut result = Vec::with_capacity(units.len());
+
+    let first_value = ft.first().unwrap_or_else(|| {
+        unreachable!("there has been a check on the emptyness of values before")
+    });
+    result.push(first_value.mul(&inv_len));
+
+    for fv in ft.iter().skip(1).rev() {
+        result.push(fv.mul(&inv_len));
+    }
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        primefield::PrimeFieldElement,
+        stark::fft::{fft, inverse_fft},
+    };
+
+    #[test]
+    fn test_fft() {
+        const N: u32 = 337;
+        let g = PrimeFieldElement::<N>::from(85);
+        let mut units = vec![PrimeFieldElement::<N>::from(1)];
+        let mut power_of_g = g;
+        while power_of_g != 1.into() {
+            units.push(power_of_g);
+            power_of_g = power_of_g.mul(&g);
+        }
+
+        for values in [
+            vec![3, 1, 4, 1, 5, 9, 2, 6],
+            vec![1, 0, 1, 0, 1, 0],
+            vec![1, 0, 1, 0, 1],
+            vec![1u32, 0, 0, 0],
+        ] {
+            let mut values: Vec<PrimeFieldElement<N>> = values
+                .iter()
+                .map(|v| PrimeFieldElement::<N>::from(*v))
+                .collect();
+            let ft = fft(&values, &units);
+
+            values.resize(units.len(), 0.into());
+            assert_eq!(inverse_fft(&ft, &units).unwrap(), values);
+        }
+    }
 }
